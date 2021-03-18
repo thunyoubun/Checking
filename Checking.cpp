dd9e113be31e45728226bce0d1a6a8bbabc034a3
Switch <thunyoubun@gmail.com>
2021-03-18 15:46:52 +0700

    ch

--- Checking.cpp
+++ Checking.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+//#include <conio.h>
+// #include<string>
+// #ifdef _WIN32
+// #include <Windows.h>
+// #else
+// #include <unistd.h>
+// #endif
+
 using namespace std;
 
-//จงเขียน ฟังก์ชัน int findWinnner() ที่รับ ค่า arrays 2มิติ  และ cout หมายเลขของผู้เข้าแข่งขันที่ชนะ และคะแนนรวมที่เขาได้รับ
+
+
+//จงเขียน ฟังก์ชัน int findWinnner() ที่รับค่า arrays 2มิติ  และ cout หมายเลขของผู้เข้าแข่งขันที่ชนะ และคะแนนรวมที่เขาได้รับ
 
 int findWinner(int sc[][4]){
 
@@ -20,6 +30,10 @@ int findWinner(int sc[][4]){
 }
 
 
+
+
+
+ 
 //// TEST ////
 
 int findDinner(int sc [][4]){
@@ -35,9 +49,8 @@ int findDinner(int sc [][4]){
         }
     }
     
-    cout << ans+1 << " " << max;
+    cout <<  ans+1 << " " << max;
     return 0;
-    
 }
 
 int checkAns(int sc [][4]){
@@ -52,11 +65,49 @@ int checkAns(int sc [][4]){
     return 0;
 }
 
+void showMatrix(int  sc [][4]){
+	for(int i=0; i<5; i++)
+	{
+	for(int j =0 ; j<4; j++)
+	{
+		cout << sc[i][j] << " ";
+
+	}
+	cout << "\n";
+	}
+}
+
+
 
 
 int main(){
     int sc[][4] = {5, 4, 4, 5, 5, 4, 4, 4,5, 5, 4, 4,5, 5, 5, 4,4 ,4, 4, 5};
     checkAns(sc);
+
+
+
+//    string s = "Press 'Y'  for CHECK...";
+//     for(int j=0; j< s. size( ) ; j ++)
+// 	{
+//        Sleep(100);
+// 		for(int i=0; i<=100 ; i++) ;
+// 		cout<<s[j];
+
+// 	}
+//     cout << "\n";
+//     char dir;
+//     dir = getch();
+
+//     if(dir == 'y'){
+//         cout << "\b" << " " << "\b";
+//         showMatrix(sc);
+//         checkAns(sc);
+//     }
+    
+    
+   
+   
+
+    
     return 0;
 }	
-
